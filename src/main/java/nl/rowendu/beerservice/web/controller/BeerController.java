d43f799b254e1f9f92bf882597dbd05b3531c160
ronlangeveld <ron66langeveld@gmail.com>
2023-05-04 21:09:09 +0200

    Adding beer controller. Close #3

---
+++ src/main/java/nl/rowendu/beerservice/web/controller/BeerController.java
@@ -0,0 +1,37 @@
+package nl.rowendu.beerservice.web.controller;
+
+import nl.rowendu.beerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+/** Created by ronlangeveld on 04/05/2023 */
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+  @GetMapping("{beerId}")
+  public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+
+    // todo impl
+    return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<Serializable> savedNewBeer(@RequestBody BeerDto beerDto) {
+
+    // todo impl
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{beerId}")
+  public ResponseEntity<Serializable> updateBeerByid(
+      @PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+
+    // todo impl
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+}
