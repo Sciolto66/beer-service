5db77d306d03f437e64bbd59116ce0be28068a37
ronlangeveld <ron66langeveld@gmail.com>
2023-05-04 20:07:08 +0200

    Initial project add. Close #1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# beer-service
-Beer Service
+# Beer Service
+
+Spring Boot Microservice example
---
+++ src/test/java/nl/rowendu/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package nl.rowendu.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
