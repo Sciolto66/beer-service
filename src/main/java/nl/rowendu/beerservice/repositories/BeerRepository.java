5360c60612a070d00104908d8bd49a5be2677345
ronlangeveld <ron66langeveld@gmail.com>
2023-05-07 23:46:25 +0200

    Adding bootstrap. Close #7

--- src/main/java/nl/rowendu/beerservice/repositories/BeerRepository.java
+++ src/main/java/nl/rowendu/beerservice/repositories/BeerRepository.java
@@ -1,6 +1,7 @@
 package nl.rowendu.beerservice.repositories;
 
 import nl.rowendu.beerservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
@@ -9,6 +10,6 @@ import java.util.UUID;
  * Created by ronlangeveld on 07/05/2023
  */
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
 }
