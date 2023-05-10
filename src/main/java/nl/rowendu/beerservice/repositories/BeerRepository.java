c260503b15e860cc8ae3561d9452fb5a789b8dd7
ronlangeveld <ron66langeveld@gmail.com>
2023-05-10 23:48:24 +0200

    Adding validation. Close #8

--- src/main/java/nl/rowendu/beerservice/repositories/BeerRepository.java
+++ src/main/java/nl/rowendu/beerservice/repositories/BeerRepository.java
@@ -2,7 +2,6 @@ package nl.rowendu.beerservice.repositories;
 
 import nl.rowendu.beerservice.domain.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
