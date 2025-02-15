7eceb2a6ee4325fdf029fee1f59f11fd02c6e8ca
ronlangeveld <ron66langeveld@gmail.com>
2025-02-15 16:32:34 +0100

    automated review

--- src/main/java/nl/rowendu/beerservice/web/controller/BeerController.java
+++ src/main/java/nl/rowendu/beerservice/web/controller/BeerController.java
@@ -1,16 +1,21 @@
 package nl.rowendu.beerservice.web.controller;
 
-import lombok.RequiredArgsConstructor;
-import nl.rowendu.beerservice.repositories.BeerRepository;
-import nl.rowendu.beerservice.services.BeerService;
-import nl.rowendu.beerservice.web.mapper.BeerMapper;
-import nl.rowendu.beerservice.web.model.BeerDto;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import nl.rowendu.beerservice.services.BeerService;
+import nl.rowendu.beerservice.web.model.BeerDto;
 
 /**
  * Created by ronlangeveld on 04/05/2023
@@ -28,14 +33,15 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
         return new ResponseEntity<>(beerService.saveNewBeer(beerDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerByid(
+    public ResponseEntity<Void> updateBeerById(
             @PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto) {
-        return new ResponseEntity<>(beerService.updateBeer(beerId, beerDto), HttpStatus.NO_CONTENT);
+        beerService.updateBeer(beerId, beerDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
